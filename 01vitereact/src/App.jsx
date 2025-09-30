b936bf61c053478f0d5e1db30d4183b9e75f6c2f
sampark_31 <samparksagarbarik1@gmail.com>
2025-09-30 09:59:59 +0530

    start

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+import Chai from "./chai"
+
+function App() {
+ 
+
+  return (
+    <>
+  
+    <Chai />
+    <h1>Nigga</h1>
+    </>
+  )
+}
+
+export default App;
---
+++ CustomReact/customreact.js
@@ -0,0 +1,19 @@
+import { Children } from "react"
+
+const reactElement = {
+  type:'a',
+  props:{
+    href:'https://google.com',
+    target:'_blank'
+  },
+  Children:'Click me to visit google'
+}
+
+
+
+
+
+const maincontainer = document.querySelector("#root")
+
+
+
