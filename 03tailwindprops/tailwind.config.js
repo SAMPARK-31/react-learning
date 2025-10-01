80b3b4aec735e6b0d28a753f263178cccb6725e3
sampark_31 <samparksagarbarik1@gmail.com>
2025-10-01 19:14:22 +0530

    day 2

---
+++ 03tailwindprops/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+   content: [
+    "./index.html",        // your main HTML file
+    "./src/**/*.{js,ts,jsx,tsx}",  // all JS/TS/React files inside src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
